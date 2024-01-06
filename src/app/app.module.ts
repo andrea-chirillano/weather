39730b94d4dadc078ac88b1dec2d0f239da56342
Andrea Chirillano <andreatchirillano@hotmail.com>
2024-01-06 03:11:47 -0300

    Add: sunny weather background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VisualWeatherComponent } from './visual-weather/visual-weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisualWeatherComponent
   ],
   imports: [
     BrowserModule,
