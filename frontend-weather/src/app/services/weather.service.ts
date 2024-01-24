6473ab6e713128dfb5575bc8e2e96bb8dd909380
Andrea Chirillano <andreatchirillano@hotmail.com>
2024-01-24 16:49:37 -0300

    Branch frontend

--- frontend-weather/src/app/services/weather.service.ts
+++ frontend-weather/src/app/services/weather.service.ts
@@ -1,18 +1,323 @@
-// En tu servicio, por ejemplo, weather.service.ts
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { formatDate } from '@angular/common';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  private apiUrl = `http://localhost:8000/api/weather`; 
-  constructor(private http: HttpClient) {}
+  private apiKey = 'd0b424f9a7e664168950846de81ef193';
+  private dayOfWeek: string = "";
+  private timeOfDay: string = "";
+
+  private countryDict: { [code: string]: string } = {
+    'AD': "Andorra",
+    'AE': "United Arab Emirates",
+    'AF': "Afghanistan",
+    'AG': "Antigua and Barbuda",
+    'AI': "Anguilla",
+    'AL': "Albania",
+    'AM': "Armenia",
+    'AO': "Angola",
+    'AQ': "Antarctica",
+    'AR': "Argentina",
+    'AS': "American Samoa",
+    'AT': "Austria",
+    'AU': "Australia",
+    'AW': "Aruba",
+    'AX': "Åland Islands",
+    'AZ': "Azerbaijan",
+    'BA': "Bosnia and Herzegovina",
+    'BB': "Barbados",
+    'BD': "Bangladesh",
+    'BE': "Belgium",
+    'BF': "Burkina Faso",
+    'BG': "Bulgaria",
+    'BH': "Bahrain",
+    'BI': "Burundi",
+    'BJ': "Benin",
+    'BL': "Saint Barthélemy",
+    'BM': "Bermuda",
+    'BN': "Brunei Darussalam",
+    'BO': "Bolivia (Plurinational State of)",
+    'BQ': "Bonaire, Sint Eustatius and Saba",
+    'BR': "Brazil",
+    'BS': "Bahamas",
+    'BT': "Bhutan",
+    'BV': "Bouvet Island",
+    'BW': "Botswana",
+    'BY': "Belarus",
+    'BZ': "Belize",
+    'CA': "Canada",
+    'CC': "Cocos (Keeling) Islands",
+    'CD': "Congo, Democratic Republic of the",
+    'CF': "Central African Republic",
+    'CG': "Congo",
+    'CH': "Switzerland",
+    'CI': "Côte d'Ivoire",
+    'CK': "Cook Islands",
+    'CL': "Chile",
+    'CM': "Cameroon",
+    'CN': "China",
+    'CO': "Colombia",
+    'CR': "Costa Rica",
+    'CU': "Cuba",
+    'CV': "Cabo Verde",
+    'CW': "Curaçao",
+    'CX': "Christmas Island",
+    'CY': "Cyprus",
+    'CZ': "Czechia",
+    'DE': "Germany",
+    'DJ': "Djibouti",
+    'DK': "Denmark",
+    'DM': "Dominica",
+    'DO': "Dominican Republic",
+    'DZ': "Algeria",
+    'EC': "Ecuador",
+    'EE': "Estonia",
+    'EG': "Egypt",
+    'EH': "Western Sahara",
+    'ER': "Eritrea",
+    'ES': "Spain",
+    'ET': "Ethiopia",
+    'FI': "Finland",
+    'FJ': "Fiji",
+    'FK': "Falkland Islands (Malvinas)",
+    'FM': "Micronesia (Federated States of)",
+    'FO': "Faroe Islands",
+    'FR': "France",
+    'GA': "Gabon",
+    'GB': "United Kingdom of Great Britain and Northern Ireland",
+    'GD': "Grenada",
+    'GE': "Georgia",
+    'GF': "French Guiana",
+    'GG': "Guernsey",
+    'GH': "Ghana",
+    'GI': "Gibraltar",
+    'GL': "Greenland",
+    'GM': "Gambia",
+    'GN': "Guinea",
+    'GP': "Guadeloupe",
+    'GQ': "Equatorial Guinea",
+    'GR': "Greece",
+    'GS': "South Georgia and the South Sandwich Islands",
+    'GT': "Guatemala",
+    'GU': "Guam",
+    'GW': "Guinea-Bissau",
+    'GY': "Guyana",
+    'HK': "Hong Kong",
+    'HM': "Heard Island and McDonald Islands",
+    'HN': "Honduras",
+    'HR': "Croatia",
+    'HT': "Haiti",
+    'HU': "Hungary",
+    'ID': "Indonesia",
+    'IE': "Ireland",
+    'IL': "Israel",
+    'IM': "Isle of Man",
+    'IN': "India",
+    'IO': "British Indian Ocean Territory",
+    'IQ': "Iraq",
+    'IR': "Iran (Islamic Republic of)",
+    'IS': "Iceland",
+    'IT': "Italy",
+    'JE': "Jersey",
+    'JM': "Jamaica",
+    'JO': "Jordan",
+    'JP': "Japan",
+    'KE': "Kenya",
+    'KG': "Kyrgyzstan",
+    'KH': "Cambodia",
+    'KI': "Kiribati",
+    'KM': "Comoros",
+    'KN': "Saint Kitts and Nevis",
+    'KP': "Korea (Democratic People's Republic of)",
+    'KR': "Korea, Republic of",
+    'KW': "Kuwait",
+    'KY': "Cayman Islands",
+    'KZ': "Kazakhstan",
+    'LA': "Lao People's Democratic Republic",
+    'LB': "Lebanon",
+    'LC': "Saint Lucia",
+    'LI': "Liechtenstein",
+    'LK': "Sri Lanka",
+    'LR': "Liberia",
+    'LS': "Lesotho",
+    'LT': "Lithuania",
+    'LU': "Luxembourg",
+    'LV': "Latvia",
+    'LY': "Libya",
+    'MA': "Morocco",
+    'MC': "Monaco",
+    'MD': "Moldova, Republic of",
+    'ME': "Montenegro",
+    'MF': "Saint Martin (French part)",
+    'MG': "Madagascar",
+    'MH': "Marshall Islands",
+    'MK': "North Macedonia",
+    'ML': "Mali",
+    'MM': "Myanmar",
+    'MN': "Mongolia",
+    'MO': "Macao",
+    'MP': "Northern Mariana Islands",
+    'MQ': "Martinique",
+    'MR': "Mauritania",
+    'MS': "Montserrat",
+    'MT': "Malta",
+    'MU': "Mauritius",
+    'MV': "Maldives",
+    'MW': "Malawi",
+    'MX': "Mexico",
+    'MY': "Malaysia",
+    'MZ': "Mozambique",
+    'NA': "Namibia",
+    'NC': "New Caledonia",
+    'NE': "Niger",
+    'NF': "Norfolk Island",
+    'NG': "Nigeria",
+    'NI': "Nicaragua",
+    'NL': "Netherlands, Kingdom of the",
+    'NO': "Norway",
+    'NP': "Nepal",
+    'NR': "Nauru",
+    'NU': "Niue",
+    'NZ': "New Zealand",
+    'OM': "Oman",
+    'PA': "Panama",
+    'PE': "Peru",
+    'PF': "French Polynesia",
+    'PG': "Papua New Guinea",
+    'PH': "Philippines",
+    'PK': "Pakistan",
+    'PL': "Poland",
+    'PM': "Saint Pierre and Miquelon",
+    'PN': "Pitcairn",
+    'PR': "Puerto Rico",
+    'PS': "Palestine, State of",
+    'PT': "Portugal",
+    'PW': "Palau",
+    'PY': "Paraguay",
+    'QA': "Qatar",
+    'RE': "Réunion",
+    'RO': "Romania",
+    'RS': "Serbia",
+    'RU': "Russian Federation",
+    'RW': "Rwanda",
+    'SA': "Saudi Arabia",
+    'SB': "Solomon Islands",
+    'SC': "Seychelles",
+    'SD': "Sudan",
+    'SE': "Sweden",
+    'SG': "Singapore",
+    'SH': "Saint Helena, Ascension and Tristan da Cunha",
+    'SI': "Slovenia",
+    'SJ': "Svalbard and Jan Mayen",
+    'SK': "Slovakia",
+    'SL': "Sierra Leone",
+    'SM': "San Marino",
+    'SN': "Senegal",
+    'SO': "Somalia",
+    'SR': "Suriname",
+    'SS': "South Sudan",
+    'ST': "Sao Tome and Principe",
+    'SV': "El Salvador",
+    'SX': "Sint Maarten (Dutch part)",
+    'SY': "Syrian Arab Republic",
+    'SZ': "Eswatini",
+    'TC': "Turks and Caicos Islands",
+    'TD': "Chad",
+    'TF': "French Southern Territories",
+    'TG': "Togo",
+    'TH': "Thailand",
+    'TJ': "Tajikistan",
+    'TK': "Tokelau",
+    'TL': "Timor-Leste",
+    'TM': "Turkmenistan",
+    'TN': "Tunisia",
+    'TO': "Tonga",
+    'TR': "Türkiye",
+    'TT': "Trinidad and Tobago",
+    'TV': "Tuvalu",
+    'TW': "Taiwan, Province of China",
+    'TZ': "Tanzania, United Republic of",
+    'UA': "Ukraine",
+    'UG': "Uganda",
+    'UM': "United States Minor Outlying Islands",
+    'US': "United States of America",
+    'UY': "Uruguay",
+    'UZ': "Uzbekistan",
+    'VA': "Holy See",
+    'VC': "Saint Vincent and the Grenadines",
+    'VE': "Venezuela (Bolivarian Republic of)",
+    'VG': "Virgin Islands (British)",
+    'VI': "Virgin Islands (U.S.)",
+    'VN': "Viet Nam",
+    'VU': "Vanuatu",
+    'WF': "Wallis and Futuna",
+    'WS': "Samoa",
+    'YE': "Yemen",
+    'YT': "Mayotte",
+    'ZA': "South Africa",
+    'ZM': "Zambia",
+  };
+
+  constructor(private http: HttpClient) { }
+
+  private getCountryName(countryCode: string): string {
+    return this.countryDict[countryCode] || 'Unknown country';
+  }
+
+
+  getTimeInfo(city: string, country: string): string {
+    const currentDate = new Date();
+    formatDate(currentDate, 'full', 'en-US', `${city}, ${country}`);
+
+    // Obtener el día de la semana y la hora actual en formato localizado
+    this.dayOfWeek = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(currentDate);
+    this.timeOfDay = new Intl.DateTimeFormat('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }).format(currentDate);
+    const timeFormat = this.dayOfWeek + ", " + this.timeOfDay 
+    return timeFormat
+  }
+
+
+  private getExactTime(timestamp: number, city: string, country: string): string {
+    if (city != null && country != null) {
+      return this.getTimeInfo(city, country);
+    } else {
+      return 'Unknown time';
+    }
+  }
+
+  
 
   getWeather(countryCode: string, cityInput: string): Observable<any> {
-    const params = { countryCode, cityInput };
-    return this.http.get<any>(this.apiUrl, { params });
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput},${countryCode}&appid=${this.apiKey}`;
+    
+    return this.http.get(url);
+  }
+
+  formatWeatherData(data: any): any {
+    const countryName = this.getCountryName(data?.sys?.country);
+    const exactTime = this.getExactTime(data?.dt, data?.name, countryName);
+    const mainCondition = data?.weather[0]?.main.toLowerCase();
+    const temperatureCelsius = Math.round(data?.main?.temp - 273.15);
+    const windSpeedMetersPerSecond = data?.wind?.speed;
+    const precipitationProbabilityPercentage = data?.clouds?.all;
+    const humidityPercentage = data?.main?.humidity;
+    const windSpeedKmh = Math.round(windSpeedMetersPerSecond * 3.6);
+
+    return {
+      countryName,
+      cityName: data?.name,
+      exactTime,
+      weather: mainCondition.charAt(0).toUpperCase() + mainCondition.slice(1),
+      temperatureCelsius,
+      precipitationProbability: precipitationProbabilityPercentage,
+      humidity: humidityPercentage,
+      windSpeed: windSpeedKmh,
+      datetime: data?.dt,
+    };
   }
 }
--- frontend-weather/src/app/visual-weather/visual-weather.component.ts
+++ frontend-weather/src/app/visual-weather/visual-weather.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { WeatherService } from '../services/weather.service';
 import { HttpClient } from '@angular/common/http';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-visual-weather',
@@ -12,7 +13,7 @@ export class VisualWeatherComponent {
   showWeatherInfo: boolean = false;
   showFormElements: boolean = true;
   iconSunny: string = '../../assets/icons/sunny.svg';
-  
+
   selectedCountry: string = '';
   cityInput: string = '';
 
@@ -25,6 +26,7 @@ export class VisualWeatherComponent {
   humidity: number = 0;
   windSpeed: number = 0;
 
+
   constructor(private fb: FormBuilder, private weatherService: WeatherService, private http: HttpClient) {
     this.weatherForm = this.fb.group({
       countryCode: ['', Validators.required],
@@ -36,48 +38,53 @@ export class VisualWeatherComponent {
     if (event) {
       event.preventDefault();
     }
-  
+
     // Get the form values (country and city)
     const countryCode = this.weatherForm.get('countryCode')?.value;
     const cityInput = this.weatherForm.get('cityInput')?.value;
-  
+
     // Print the values to verify
     console.log('Country Code:', countryCode);
     console.log('City Input:', cityInput);
-  
+
     // Verify that the values are not null before making the HTTP request
     if (!countryCode || !cityInput) {
       console.error('Country code or city input is null.');
       return;
     }
-  
+
     // Make the HTTP request to the server
-    this.weatherService.getWeather(countryCode, cityInput).subscribe(
-      (data) => {
-        // Verify that the data received is valid before assigning it
-        if (data) {
-          this.selectedCountry = data.country_name;
-          this.cityInput = data.city_name;
-          this.exactTime = data.exact_time;
-          this.weather = data.weather;
-          this.temperature = data.temperature_celsius;
-          this.precipitationProbability = data.precipitation_probability;
-          this.humidity = data.humidity;
-          this.windSpeed = data.wind_speed;
-  
+    this.weatherService.getWeather(countryCode, cityInput)
+      .subscribe({
+        next: (data) => {
+          const formattedData = this.weatherService.formatWeatherData(data);
+
+          this.selectedCountry = formattedData.countryName || 'Unknown country';
+          this.cityInput = formattedData.cityName || 'Unknown city';
+          this.exactTime = formattedData.exactTime || 'Unknown time';
+          this.weather = formattedData.weather || 'Unknown weather';
+          this.temperature = formattedData.temperatureCelsius || 0;
+          this.precipitationProbability = formattedData.precipitationProbability || 0;
+          this.humidity = formattedData.humidity || 0;
+          this.windSpeed = formattedData.windSpeed || 0;
+
           // Change the status to display information after receiving the response
           this.showWeatherInfo = true;
-          
+
           // Hide form elements after receiving response
           this.showFormElements = false;
-        } else {
-          console.error('Received invalid data from the server.');
+
+          console.log('Received data:', data);
         }
-      },
-      (error) => {
-        console.error('Error fetching weather data:', error);
-      }
-    );
+        ,
+        error: (error) => {
+          console.error('Error fetching weather data:', error);
+        },
+        complete: () => {
+          // Lógica cuando la operación está completa (equivalente a la función complete anterior)
+          console.log('Weather data fetching complete');
+        }
+    });
   }
-  
+
 }
